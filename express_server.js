2967ddbfe95388af953dffd001335dfcdd3d795b
Kevin Levasseur <kevin_levasseur@telus.net>
2024-01-30 16:49:55 -0800

    added function to generate random short URL

--- express_server.js
+++ express_server.js
@@ -10,6 +10,24 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  let string = "";
+  while (string.length < 6) {
+    const randomNum = Math.floor(Math.random() * 62); // generates a random number between 0 and 61 (let 0-9 be numbers, 10-35 be upper case letters, 36-61 be lower case letters)
+    if (randomNum < 10) {
+      const num = randomNum + 48; // utf16 numbers start at 48
+      string += String.fromCodePoint(num);
+    } else if (randomNum < 36) {
+      const num = randomNum + 55; // utf16 upper case letters start at 65 (10 + 55)
+      string += String.fromCodePoint(num);
+    } else {
+      const num = randomNum + 61; // utf16 lower case letters start at 97 (61 + 36)
+      string += String.fromCodePoint(num);
+    }
+  }
+  return string;
+};
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
