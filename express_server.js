7d48913754ef56c97093dc3a92bbb44c0afa1d08
Kevin Levasseur <kevin_levasseur@telus.net>
2024-02-05 09:51:31 -0800

    get route renders registration page

--- express_server.js
+++ express_server.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
@@ -8,8 +8,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const urlDatabase = { // default database when server is started TODO: move database into its own file and use fs to update it
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  'b2xVn2': 'http://www.lighthouselabs.ca',
+  '9sm5xK': 'http://www.google.com'
 };
 
 const generateRandomString = function() { // used to generate short URL id
@@ -30,16 +30,16 @@ const generateRandomString = function() { // used to generate short URL id
   return string;
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get('/', (req, res) => {
+  res.send('Hello!');
 });
 
-app.get("/urls.json", (req, res) => {
+app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
 app.get('/urls', (req, res) => { // renders index page when requested
@@ -50,14 +50,21 @@ app.get('/urls', (req, res) => { // renders index page when requested
   res.render('urls_index', templateVars);
 });
 
-app.get("/urls/new", (req, res) => { // renders page to create new id when requested
+app.get('/urls/new', (req, res) => { // renders page to create new id when requested
   const templateVars = {
     username: req.cookies['username']
   };
-  res.render("urls_new", templateVars);
+  res.render('urls_new', templateVars);
 });
 
-app.get("/u/:id", (req, res) => { // redirects to longURL when requested for short id, if it exists. otherwise shows 404 message
+app.get('/register', (req, res) => { // renders page to register for an account
+  const templateVars = {
+    username: req.cookies['username']
+  };
+  res.render('register', templateVars);
+})
+
+app.get('/u/:id', (req, res) => { // redirects to longURL when requested for short id, if it exists. otherwise shows 404 message
   if (!urlDatabase[req.params.id]) {
     res.send("404: The requested page doesn't exist");
   } else {
@@ -66,38 +73,38 @@ app.get("/u/:id", (req, res) => { // redirects to longURL when requested for sho
   }
 });
 
-app.get("/urls/:id", (req, res) => { // handles get request to render page with generated id
+app.get('/urls/:id', (req, res) => { // handles get request to render page with generated id
   const templateVars = {
     username: req.cookies['username'],
     id: req.params.id,
     longURL: urlDatabase[req.params.id]
   };
-  res.render("urls_show", templateVars);
+  res.render('urls_show', templateVars);
 });
 // stretch TODO: check if URL already exists in database
-app.post("/urls", (req, res) => { // creates short id for given URL and redirects to new page to show result
+app.post('/urls', (req, res) => { // creates short id for given URL and redirects to new page to show result
   const shortURLid = generateRandomString();
   urlDatabase[shortURLid] = req.body.longURL;
   res.redirect(`/urls/${shortURLid}`);
 });
 
-app.post("/urls/:id/delete", (req, res) => { // handles post request to delete id from database then reroutes to index
+app.post('/urls/:id/delete', (req, res) => { // handles post request to delete id from database then reroutes to index
   delete urlDatabase[req.params.id];
   res.redirect('/urls');
 });
 
-app.post("/urls/:id", (req, res) => { // updates database with new URL for given ID
+app.post('/urls/:id', (req, res) => { // updates database with new URL for given ID
   urlDatabase[req.params.id] = req.body.longURL;
   res.redirect('/urls');
 });
 
-app.post("/login", (req, res) => { // stores cookie with user login name
-  res.cookie("username", req.body.username);
+app.post('/login', (req, res) => { // stores cookie with user login name
+  res.cookie('username', req.body.username);
   res.redirect('/urls');
 });
 
-app.post("/logout", (req, res) => { // clears cookie storing username when logout
-  res.clearCookie("username");
+app.post('/logout', (req, res) => { // clears cookie storing username when logout
+  res.clearCookie('username');
   res.redirect('/urls');
 });
 
